07cf95904fff885ffa238600384b2dfd22514cf8
poomezz <thansukg@gmail.com>
2024-12-19 01:44:22 +0700

    Refactor modal Work, Add Modal Line, Location, DeviceType, DeviceName

--- models.py
+++ models.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
 
+
 # สร้างตัวแปร db เพื่อใช้ในการสร้างฐานข้อมูล
 db = SQLAlchemy()
+
 
 # User Model
 class User(UserMixin, db.Model):
@@ -16,18 +18,64 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-# Work Model
+# Model สำหรับ Work
 class Work(db.Model):
-    number = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    create_date = db.Column(db.DateTime, default=datetime.utcnow)
-    work_order = db.Column(db.String(50), nullable=False)
-    equipment = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    location = db.Column(db.String(50), nullable=False)
-    report_by = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.String(20), nullable=False, default="open")
-    action = db.Column(db.Text, nullable=True)
-    link = db.Column(db.String(255), nullable=True)
+    number = db.Column(db.Integer, primary_key=True, autoincrement=True)  # เลข 6 หลัก รันอัตโนมัติ
+    create_date = db.Column(db.DateTime, default=datetime.utcnow)  # วันที่และเวลา สร้างอัตโนมัติ
+    work_order = db.Column(db.String(50), nullable=False)          # Work Order
+    # ForeignKey เชื่อมกับตารางอื่นๆ
+    line_id = db.Column(db.Integer, db.ForeignKey('line.id'), nullable=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=True)
+    device_type_id = db.Column(db.Integer, db.ForeignKey('device_type.id'), nullable=True)
+    device_name_id = db.Column(db.Integer, db.ForeignKey('device_name.id'), nullable=True)
+    description = db.Column(db.Text, nullable=False)               # รายละเอียด
+    report_by = db.Column(db.String(50), nullable=False)           # ผู้รายงาน
+    status = db.Column(db.String(20), nullable=False, default="open")  # สถานะ (ค่าเริ่มต้น: open)
+    #action = db.Column(db.Text, nullable=True)                     # รายละเอียดของการดำเนินการ
+    link = db.Column(db.String(255), nullable=True)           # URL ของไฟล์
+
+    # สร้างความสัมพันธ์
+    line = db.relationship('Line', backref='works')
+    location = db.relationship('Location', backref='works')
+    device_type = db.relationship('DeviceType', backref='works')
+    device_name = db.relationship('DeviceName', backref='works')
 
     def __repr__(self):
         return f"<Work {self.number}>"
+
+
+class Line(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<Line {self.name}>"
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    line_id = db.Column(db.Integer, db.ForeignKey('line.id'), nullable=False)
+    line = db.relationship('Line', backref=db.backref('locations', lazy=True))
+
+    def __repr__(self):
+        return f"<Location {self.name}>"
+
+class DeviceType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    line_id = db.Column(db.Integer, db.ForeignKey('line.id'), nullable=False)
+    line = db.relationship('Line', backref=db.backref('device_types', lazy=True))
+
+    def __repr__(self):
+        return f"<DeviceType {self.name}>"
+    
+class DeviceName(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    device_type_id = db.Column(db.Integer, db.ForeignKey('device_type.id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+    device_type = db.relationship('DeviceType', backref=db.backref('device_names', lazy=True))
+    location = db.relationship('Location', backref=db.backref('device_names', lazy=True))
+
+    def __repr__(self):
+        return f"<DeviceName {self.name}>"
