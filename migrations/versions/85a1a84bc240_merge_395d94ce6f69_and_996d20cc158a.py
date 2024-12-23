7d8390f7ce85974a6318cfc16e47f39e78ea094c
ThanapoomS <your.email@example.comthanapooms@bts.co.th>
2024-12-23 19:00:39 +0700

    fix db

---
+++ migrations/versions/85a1a84bc240_merge_395d94ce6f69_and_996d20cc158a.py
@@ -0,0 +1,24 @@
+"""Merge 395d94ce6f69 and 996d20cc158a
+
+Revision ID: 85a1a84bc240
+Revises: 
+Create Date: 2024-12-23 18:51:41.108553
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '85a1a84bc240'
+down_revision = ('395d94ce6f69', '996d20cc158a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
