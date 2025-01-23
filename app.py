1fa5b6cd097ab28e26ca115f89e34fa4d3169ecb
ThanapoomS <ThanapoomS@bts.co.th>
2025-01-23 19:39:30 +0700

    update app.py to run specific ip

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ init_app(app)
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host="172.20.10.14", port=5000, debug=True)
+    app.run(host="10.129.150.35", port=5000, debug=True)
 
     
